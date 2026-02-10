c74e5def22dc68db60cb40b1e9357d9f446d0a8e
SonicRaptor <sonicraptor2105@gmail.com>
2026-02-10 19:18:13 +1100

    finished blob compression demo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import ctypes
 import hashlib
+import zlib
 
 def createGit():
     directory = f"{os.curdir}/.xgit"
@@ -12,18 +13,21 @@ def createGit():
     return
 
 def blobFile(f):
-    data = f.read().encode('utf-8')
+    data = f.read()
     header = f"BLOB {len(data)}\0".encode('utf-8')
     print(header)
-    finalData = header + data
+    finalData = zlib.compress(header + data)
     print(finalData)
     finalHash = hashlib.sha1(finalData).hexdigest()
     print(finalHash)
+    os.makedirs(f"{os.curdir}/.xgit/{finalHash[:2]}")
+    with open(f"{os.curdir}/.xgit/{finalHash[:2]}/{finalHash}", 'wb') as blob: blob.write(finalData)
     return
 
 createGit()
+
 with os.scandir(os.curdir) as files:
     for file in files:
         if file.is_file():
-            with open(file, 'r') as fileObj:
+            with open(file, 'rb') as fileObj:
                 blobFile(fileObj)
