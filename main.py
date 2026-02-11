1b57343cd461532c0e5fd84f6ccc95ae6a0e84f3
SonicRaptor <sonicraptor2105@gmail.com>
2026-02-11 18:14:17 +1100

    added ignore list

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def createGit():
     directory = f"{os.curdir}/.xgit"
     if not os.path.isdir(directory):
         os.mkdir(directory)
-        print(f"Directory created at {directory}")
+        print(f" - Directory created at {directory}")
         if os.name == 'nt':
             ctypes.windll.kernel32.SetFileAttributesW(directory, 0x02)
     return
@@ -15,21 +15,34 @@ def createGit():
 def blobFile(f):
     data = f.read()
     header = f"BLOB {len(data)}\0".encode('utf-8')
-    print(header)
     finalData = zlib.compress(header + data)
-    print(finalData)
     finalHash = hashlib.sha1(finalData).hexdigest()
-    print(finalHash)
     os.makedirs(f"{os.curdir}/.xgit/{finalHash[:2]}", exist_ok=True)
     with open(f"{os.curdir}/.xgit/{finalHash[:2]}/{finalHash}", 'wb') as blob: blob.write(finalData)
     return
 
+def createIgnoreFile():
+    ignoreList = [".xgit", ".git"] #Default ignored files
+    with open(f"{os.curdir}/.xgit/ignore.dat", 'w') as f: f.write('\n'.join(ignoreList))
+    return ignoreList
+
+
+
 createGit()
+ignoreList = list()
+try:
+    with open(f"{os.curdir}/.xgit/ignore.dat", 'r') as f: ignoreList = f.read().split("\n")
+    print(ignoreList)
+except OSError:
+    print(" - Regenerating ignore.dat")
+    ignoreList = createIgnoreFile()
+
 
 with os.scandir(os.curdir) as files:
     for file in files:
-        if file.is_file():
+        if file.is_file() and not file.name in ignoreList:
+            print(f"file {file.name}")
             with open(file, 'rb') as fileObj:
                 blobFile(fileObj)
-        elif file.is_dir():
+        elif file.is_dir() and not file.name in ignoreList:
             print(f"folder {file.name}")
